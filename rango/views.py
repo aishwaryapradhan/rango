953b12ab91ec03b24193896ec00ea7d400568ab4
aishwaryapradhan <aishwaryapradhan82@gmail.com>
2017-05-20 03:48:30 +0530

    Created forms for getting input

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from rango.forms import CategoryForm, PageForm
 from .models import Category, Page
 
 
@@ -13,7 +15,7 @@ def index(request):
 
 
 def about(request):
-    context_dict = {'my_name':"Aishwarya Pradhan"}
+    context_dict = {'my_name': "Aishwarya Pradhan"}
     return render(request, "rango/about.html", context_dict)
 
 
@@ -29,3 +31,50 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
 
     return render(request, "rango/category.html", context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except category.DoesNotExist:
+        category = None
+
+    form = PageForm()
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug)
+            else:
+                print(form.errors)
+
+            return index()
+        else:
+            print(form.errors)
+
+    context_dict = {
+        'form': form,
+        'category': category,
+    }
+    return render(request, 'rango/add_page.html', context_dict)
