eca2a177af21fff008a4a4725acf536aa0672eac
aishwaryapradhan <aishwaryapradhan82@gmail.com>
2017-05-20 20:16:10 +0530

    Implemented DRY

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+
 from rango import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index,name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls'))
 ]
