bf39f3588726f8dfc2647a24691df54d0ebeef94
aishwaryapradhan <aishwaryapradhan82@gmail.com>
2017-05-25 23:11:15 +0530

    Tested sessions and cookies

--- rango/views.py
+++ rango/views.py
@@ -1,24 +1,35 @@
+from datetime import datetime
+
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
 from rango.forms import CategoryForm, PageForm, Userform, UserProfileForm
 from .models import Category, Page
 
 
 def index(request):
+    request.session.set_test_cookie()
     category_likes = Category.objects.order_by("-likes")[:5]
     view_pages = Page.objects.order_by("-views")[:5]
     context_dict = {
         'liked_categories': category_likes,
         'viewed_pages': view_pages,
     }
-    return render(request, "rango/index.html", context_dict)
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, "rango/index.html", context_dict)
+    return response
 
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("Oh lord! the test cookie worked")
+        request.session.delete_test_cookie()
     context_dict = {'my_name': "Aishwarya Pradhan"}
     return render(request, "rango/about.html", context_dict)
 
@@ -150,3 +161,24 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit']=str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
